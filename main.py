15017f31e2583ee938166b6bd90d171be2f7a07f
Tp411 <thomas.pisanne@coda-student.school>
2025-10-02 12:49:45 +0200

    ex4

--- main.py
+++ main.py
@@ -11,11 +11,18 @@ def exercice3():
     print("Afficher trois lignes")
     print(" ligne1\n ligne2\n ligne3")
 
+def exercice4():
+    print("Exercice 4 : Calculer l'âge")
+    année_naissance =int(input("entrez votre année de naissance : "))
+    année_actuelle = 2025
+    âge = année_actuelle - année_naissance
+    print(f"Vous avez {âge} ans.")
+   
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
-    if choix == "3":
-        exercice3()
+    if choix == "4":
+        exercice4()
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
