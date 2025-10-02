18b3c2c651e6b6562697c8e5cf50b25384434941
Tp411 <thomas.pisanne@coda-student.school>
2025-10-02 12:38:39 +0200

    ex3

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ def exercice2():
     prenom = input("Entrez votre prénom : ")
     print(f"Bonjour, {prenom} !")
     
+def exercice3():
+    print("Afficher trois lignes")
+    print(" ligne1\n ligne2\n ligne3")
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
-    if choix == "2":
-        exercice2()
+    if choix == "3":
+        exercice3()
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
